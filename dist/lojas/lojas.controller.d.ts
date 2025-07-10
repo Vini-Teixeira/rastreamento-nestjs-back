aae2ef8dd3a085ae2683c8552dcff3e4ae67b157
Vinicius Teixeira <teixeiravinicius32@gmail.com>
2025-07-09 22:04:31 -0300

    Mudança nome repositório para novo padrão.

--- dist/lojas/lojas.controller.d.ts
+++
@@ -1,8 +0,0 @@
-import { LojasService } from './lojas.service';
-import { CreateLojaDto } from './dto/create-loja.dto';
-export declare class LojasController {
-    private readonly lojasService;
-    constructor(lojasService: LojasService);
-    create(createLojaDto: CreateLojaDto): Promise<import("./schemas/loja.schema").Loja>;
-    findAll(): Promise<import("./schemas/loja.schema").Loja[]>;
-}
--- dist/lojas/lojas.service.d.ts
+++
@@ -1,9 +0,0 @@
-import { Model } from 'mongoose';
-import { CreateLojaDto } from './dto/create-loja.dto';
-import { Loja, LojaDocument } from './schemas/loja.schema';
-export declare class LojasService {
-    private lojaModel;
-    constructor(lojaModel: Model<LojaDocument>);
-    create(createLojaDto: CreateLojaDto): Promise<Loja>;
-    findAll(): Promise<Loja[]>;
-}
